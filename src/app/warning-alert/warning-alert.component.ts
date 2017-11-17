a468f499ef81605efd21d59784ae297dcf0c20ce
Luciano Daher <luciano.daher@gmail.com>
2017-11-17 14:40:38 -0200

    criando os components WarningAlertComponent e SuccessAlertComponent conforme enunciado do exercício

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-warning-alert]',
+  template: `
+    <h2>Warning message</h2>
+  `,
+  styleUrls: ['warning-alert.component.css']
+})
+export class WarningAlertComponent {}
