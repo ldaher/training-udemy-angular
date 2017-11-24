7032b751583e03555560acb0f0cff63f6a1ac98e
Luciano Daher <luciano.daher@gmail.com>
2017-11-24 14:19:52 -0200

    aula 'two way databinding' finalizada

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: 'app-servers',
-  // selector: '[app-servers]',
-  selector: '.app-servers',
-  // template: `
-  // <app-server></app-server>
-  // <app-server></app-server>`,
+  // selector: 'app-servers', // default
+  selector: '[app-servers]', // como um atributo HTML
+  // selector: '.app-servers', //como valor do atributo "class" de HTML
+/*   template: `
+  <app-server></app-server>
+  <app-server></app-server>`, */
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer: boolean = false;
-
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Luciano';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    }, 5000);
+    }, 2000);
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
   }
 
+  onUpdateServerName (event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
