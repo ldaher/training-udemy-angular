c20a8111528b619479a27483580e9b8f157569b8
Luciano Daher <luciano.daher@gmail.com>
2017-11-24 14:27:32 -0200

    aula 'combining all forms of databinding' finalizada

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -24,7 +24,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created. The server name is ' + this.serverName;
   }
 
   onUpdateServerName (event: Event) {
